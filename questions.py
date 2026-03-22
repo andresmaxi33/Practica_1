30a47e2b8f643629a950b0feba843b54a30e986e
Andres Sales <andressales33@gmail.com>
2026-03-22 09:49:01 -0300

    Agregado rondas sin repetir

--- questions.py
+++ questions.py
@@ -6,8 +6,6 @@ words = {
     "general": ["programa", "bucle"]
 }
 
-guessed = [] 
-attempts = 6
 score = 0
 
 print("¡Bienvenido al Ahorcado!") 
@@ -26,49 +24,55 @@ while True:
     else:
         print("Categoria no valida")
 
-word = random.choice(words[categoria])
+# Lista sin repetir palabras
+lista_palabras = random.sample(words[categoria], len(words[categoria]))
 
-while attempts > 0: 
+for word in lista_palabras:
+    
+    guessed = []
+    attempts = 6
+   
+    while attempts > 0: 
     # Mostrar progreso: letras adivinadas y guiones para las que faltan 
-    progress = "" 
-    for letter in word: 
-        if letter in guessed: 
-           progress += letter + " "
-        else: 
-            progress += "_ "
-    print(progress)
+        progress = "" 
+        for letter in word: 
+            if letter in guessed: 
+                progress += letter + " "
+            else: 
+                progress += "_ "
+        print(progress)
     
     # Verificar si el jugador ya adivinó la palabra completa 
-    if "_" not in progress: 
-        print("¡Ganaste!") 
-        score += 6
-        break
+        if "_" not in progress: 
+            print("¡Ganaste!") 
+            score += 6
+            break
     
-    print(f"Intentos restantes: {attempts}") 
-    print(f"Letras usadas: {', '.join(guessed)}") 
+        print(f"Intentos restantes: {attempts}") 
+        print(f"Letras usadas: {', '.join(guessed)}") 
     
-    letter = input("Ingresá una letra: ")
+        letter = input("Ingresá una letra: ")
     
-    #corregir entrada: debe ser una sola letra y no un numero o símbolo
-    if len(letter) != 1 or not letter.isalpha():
-        print("Entrada no válida")
-        continue
+    #corregir entrada: debe ser una sola letra y no un numero o simbolo
+        if len(letter) != 1 or not letter.isalpha():
+            print("Entrada no válida")
+            continue
     
-    if letter in guessed: 
-        print("Ya usaste esa letra.")
-    elif letter in word: 
-        guessed.append(letter) 
-        print("¡Bien! Esa letra está en la palabra.")
-    else: 
-        guessed.append(letter) 
-        attempts -= 1 
-        print("Esa letra no está en la palabra.")
-        score -= 1
+        if letter in guessed: 
+            print("Ya usaste esa letra.")
+        elif letter in word: 
+            guessed.append(letter) 
+            print("¡Bien! Esa letra está en la palabra.")
+        else: 
+            guessed.append(letter) 
+            attempts -= 1 
+            print("Esa letra no está en la palabra.")
+            score -= 1
 
-    print()
+        print()
     
-else: 
-    print(f"¡Perdiste! La palabra era: {word}")
-    score = 0
-    
+    else: 
+        print(f"¡Perdiste! La palabra era: {word}")
+        score = 0
+
 print("Puntaje:", score)
